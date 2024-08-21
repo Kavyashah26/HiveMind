b2e14e7da1e8b5fe117d7a9cdec6a69a83593804
Kavya Shah <kavyaatulshahpatan@gmail.com>
2024-08-22 04:29:30 +0530

    .gitkeep

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,9 @@ const userRoutes=Router()
 // userRoutes.post('/',[authMiddleware,])
 userRoutes.delete('/address/:id',[authMiddleware])
 userRoutes.get('/address',[authMiddleware])
-userRoutes.put('/', [authMiddleware,])
+userRoutes.put('/:userId', [authMiddleware,updateUser])
+
+
 userRoutes.get('/:userId', [authMiddleware,getUser])
 
 
