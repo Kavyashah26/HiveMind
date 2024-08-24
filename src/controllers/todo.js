83311c1bf0a0201bece49625a9e4aea612db1a0b
Arsh <arshgangani18@gmail.com>
2024-08-24 17:44:47 +0530

    added create todo controller

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,77 +1,73 @@
 // const Todo = ("../schema/todo.js");
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import Todo from "../schema/todo.js";
 
 //give all todos of a specific user
-// export const getUsersTodos = async (req, res) => {
-//     let { uId } = req.params;
-//     // const userObjectId = mongoose.Types.ObjectId(uId);
-//     const userObjectId = mongoose.Types.ObjectId(uId)
-//     const allTodo = await Todo.find({assignedTo:userObjectId});
-//     console.log(uId);
-//     console.log(allTodo);
-    
-//     if (allTodo) {
-//         res.json(allTodo);
-//     } else {
-//         res.status(404).json({ message: "Todo not found" });
-//     }
-// };
-
-// import mongoose from "mongoose";
-
 export const getUsersTodos = async (req, res) => {
-    let { uId } = req.params;
-    
-    // Use 'new' to create a new ObjectId instance
-    const userObjectId = new mongoose.Types.ObjectId(uId);
+  let { uId } = req.params;
+  const userObjectId = new mongoose.Types.ObjectId(uId);
+  const allTodo = await Todo.find({ assignedTo: userObjectId });
+  console.log(uId);
+  console.log(allTodo);
 
-    try {
-        const allTodo = await Todo.find({  });
-        console.log(userObjectId);
-        console.log(allTodo);
-
-        if (allTodo.length > 0) {
-            res.json(allTodo);
-        } else {
-            res.status(404).json({ message: "Todo not found" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error });
-    }
+  if (allTodo) {
+    res.json(allTodo);
+  } else {
+    res.status(404).json({ message: "Todo not found" });
+  }
 };
 
 //update completed by admin
-export const markCompleted = async (req,res) => {
-    let {pId} = req.params;
-    const updateTodo = await Todo.findByIdAndUpdate(pId,{Completed:True},{new:true,runValidators:true});
-    if(updateTodo) {
-        res.json({message:"Todo marked as completed",todo:updateTodo});
-    } else {
-        res.status(404).json({ message: "Todo not found" });
-    }
+export const markCompleted = async (req, res) => {
+  let { pid } = req.params;
+  let pIdObjectId = new mongoose.Types.ObjectId(pid);
+  let todoObjectId = new mongoose.Types.ObjectId(req.body.todoId);
+  const result = await Todo.updateOne(
+    { _id: todoObjectId, pId: pIdObjectId },
+    { $set: { Completed: "True" } }
+  );
+  console.log(result);
+  if (result.matchedCount) {
+    res.json({ message: "Todo marked as completed" });
+  } else {
+    res.status(404).json({ message: "Todo not found" });
+  }
 };
 
 //update waiting by member
-export const markWaiting = async(req,res) => {
-    let {pId} = req.params;
-    const updateTodo = await Todo.findByIdAndUpdate(pId,{Completed:waiting},{new:true,runValidators:true});
-    if(updateTodo) {
-        res.json({message:"Todo marked as waiting",todo:updateTodo});
-    } else {
-        res.status(404).json({ message: "Todo not found" });
-    }
+export const markWaiting = async (req, res) => {
+  let { pid } = req.params;
+  let pIdObjectId = new mongoose.Types.ObjectId(pid);
+  let todoObjectId = new mongoose.Types.ObjectId(req.body.todoId);
+  const result = await Todo.updateOne(
+    { _id: todoObjectId, pId: pIdObjectId },
+    { $set: { Completed: "waiting" } }
+  );
+  if (result.matchedCount) {
+    res.json({ message: "Todo marked as waiting",});
+  } else {
+    res.status(404).json({ message: "Todo not found" });
+  }
 };
 
 //get all project todos
-export const getAllProjectTodos = async(req,res) => {
-    let {pId} = req.params;
-    const allTodos = await Todo.find({pId:pId});
-    if(allTodos) {
-        res.json(allTodos);
-    } else {
-        res.status(404).json({ message: "Todo not found" });
-    }
-} 
- 
-export const createTodo=()=>{}  
+export const getAllProjectTodos = async (req, res) => {
+  let { pid } = req.params;
+  let pIdObjectId = new mongoose.Types.ObjectId(pid);
+  const allTodos = await Todo.find({ pId: pIdObjectId });
+  if (allTodos) {
+    res.json(allTodos);
+  } else {
+    res.status(404).json({ message: "Todo not found" });
+  }
+};
+
+export const createTodo = async (req,res) => {
+    let {pid} = req.params;
+    let pIdObjectId = new mongoose.Types.ObjectId(pid);
+    let newTodo = new Todo(req.body);
+    newTodo.assignedTo = new mongoose.Types.ObjectId(newTodo.assignedTo);
+    newTodo.pId = pid;
+    let savedTodo = await newTodo.save();
+    res.json(savedTodo);
+};
--- src/routes/todos.js
+++ src/routes/todos.js
@@ -1,22 +1,29 @@
-import { Router } from "express"
+import { Router } from "express";
 // import authMiddleware from "../middlewares/auth"
 // import adminMiddleware from "../middlewares/admin";
-import {  getUsersTodos,markCompleted,markWaiting,getAllProjectTodos,createTodo} from "../controllers/todo.js"
+import {
+  getUsersTodos,
+  markCompleted,
+  markWaiting,
+  getAllProjectTodos,
+  createTodo,
+} from "../controllers/todo.js";
 // import { errorHandler } from "../errors/error-handler"
 // import adminMiddleware from "../middlewares/admin"
-const todoRoutes=Router()
+const todoRoutes = Router();
 
 // userRoutes.post('/',[authMiddleware,])
 
-
 // userRoutes.get('/',[authMiddleware])
 
-
-todoRoutes.get('/user/:uId',[getUsersTodos])
+todoRoutes.get("/user/:uId", [getUsersTodos]);
+todoRoutes.put("/updateByAdmin/:pid", markCompleted);
 // todoRoutes.put('/updateByAdmin/:pid', [authMiddleware,adminMiddleware],errorHandler(markCompleted))
+todoRoutes.put("/updateByMember/:pid", markWaiting);
 // todoRoutes.put('/updateByMember/:pid', [authMiddleware],errorHandler(markWaiting))
+todoRoutes.get("/:pid", getAllProjectTodos);
 // todoRoutes.get('/:pId', [authMiddleware],errorHandler(getAllProjectTodos))
+todoRoutes.post("/:pid", createTodo);
 // todoRoutes.post('/:pId', [authMiddleware,adminMiddleware],errorHandler(createTodo))
-
 
 export default todoRoutes;
