5aafafe2615eda02a485df5779a5192c0407e6ee
Kavya Shah <kavyaatulshahpatan@gmail.com>
2024-08-24 13:42:14 +0530

    Added index

---
+++ index.js
@@ -0,0 +1,21 @@
+import express,{Express,Request,Response}  from "express";
+import { PORT } from "./secrets";
+import rootRoutes from "./routes";
+import { errorMiddleware } from "./middlewares/errors.js";
+import { signupSchema } from "./schema/users";
+
+const app=express();
+
+app.use(express.json())
+app.use('/api',rootRoutes)
+
+app.use(errorMiddleware)
+
+app.get('/',(req,res)=>{
+    res.send('Working')
+})
+
+app.listen(PORT,()=>{
+    console.log("App working!!");
+    
+});
