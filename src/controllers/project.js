041a3eb9dfd7cf40886b9a822b100e4e28a32a63
Arsh <arshgangani18@gmail.com>
2024-08-25 22:48:51 +0530

    Added controller in project related to  members

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -46,24 +46,24 @@ export const updateProjectDetails = async (req, res) => {
 };
 
 export const deleteProject = async (req, res) => {
-    let {pid} = req.params;
-    let deletedProject = await Project.deleteOne({id:pid});
+  let { pid } = req.params;
+  let deletedProject = await Project.deleteOne({ id: pid });
 
-    if (deletedProject) {
-        res.json({
-          message: "Deleted project",
-        });
-      } else {
-        res
-          .status(400)
-          .json({ message: "Project with the given id doesn't exist" });
-      }
+  if (deletedProject) {
+    res.json({
+      message: "Deleted project",
+    });
+  } else {
+    res
+      .status(400)
+      .json({ message: "Project with the given id doesn't exist" });
+  }
 };
 
 export const getProjectOverView = async (req, res) => {
-    let { pid } = req.params;
+  let { pid } = req.params;
 
-  let projectDetail = await Project.findById(pid,'liveUrl pName githubUrl');
+  let projectDetail = await Project.findById(pid, "liveUrl pName githubUrl");
 
   if (projectDetail) {
     res.json(projectDetail);
@@ -75,31 +75,83 @@ export const getProjectOverView = async (req, res) => {
 };
 
 export const getProjectMembers = async (req, res) => {
-    let {pid} = req.params;
+  let { pid } = req.params;
 
-    let getMemberIds = await Project.findById(pid,'teamMembers');
+  let getMemberIds = await Project.findById(pid, "teamMembers");
 
-    let teamMemberDetails = await Promise.all(
-        getMemberIds.teamMembers.map(async (mem) => {
-            let memberDetail = await User.findById(mem.teamMember, 'name');
-            if(memberDetail) {
-                return {
-                    teamMember: mem.teamMember,
-                    role: mem.roles,
-                    name: memberDetail.name,
-                };
-            } else {
-                res.json("Something went wrong");
-            }
-        })
-    );
-    
-    res.json(teamMemberDetails);
+  let teamMemberDetails = await Promise.all(
+    getMemberIds.teamMembers.map(async (mem) => {
+      let memberDetail = await User.findById(mem.teamMember, "name");
+      if (memberDetail) {
+        return {
+          teamMember: mem.teamMember,
+          role: mem.roles,
+          name: memberDetail.name,
+        };
+      } else {
+        res.json("Something went wrong");
+      }
+    })
+  );
 
+  res.json(teamMemberDetails);
 };
 
-export const addProjectMembers = async (req, res) => {};
+export const addProjectMembers = async (req, res) => {
+  let { pid } = req.params;
+  let memberId = req.body.memberId;
 
-export const giveRoleToProjectMembers = async (req, res) => {};
+  let addMember = await Project.findByIdAndUpdate(
+    pid,
+    { $push: { teamMembers: { teamMember: memberId } } },
+    { runValidators: true, new: true }
+  );
 
-export const removeProjectMembers = async (req, res) => {};
+  if (addMember) {
+    res.json(addMember);
+  } else {
+    res
+      .status(400)
+      .json({ message: "Project with the given id doesn't exist" });
+  }
+};
+
+export const giveRoleToProjectMembers = async (req, res) => {
+  let { pid } = req.params;
+  let {memberId, roles} = req.body;
+
+  let project = await Project.findById(pid);
+
+  if(!project) {
+    res.status(400).json({message:"project not found"});
+  }
+
+  let teamMember = project.teamMembers.find((tm)=>tm.teamMember.equals(memberId));
+  
+  if(!teamMember) {
+    res.status(400).json({message:"member not found"});
+  }
+
+  teamMember.roles = roles;
+
+  let updatedProject = await project.save();
+
+  if(!updatedProject) {
+    res.status(400).json({message:"role is not assigned"});
+  }
+
+  res.json(updatedProject);
+};
+
+export const removeProjectMembers = async (req, res) => {
+    let {pid} = req.params;
+    let {memberId} = req.body;
+
+    let updatedProject = await Project.findByIdAndUpdate(pid,{$pull:{teamMembers:{teamMember:memberId}}},{runValidators:true,new:true});
+
+    if(updatedProject) {
+        res.json(updatedProject);
+    } else {
+        res.status(400).json({message:"Something went wrong"});
+    }
+};
