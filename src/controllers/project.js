9d79ad2c4726fc3c589244261ccd7a17c5e23b32
Arsh <arshgangani18@gmail.com>
2024-09-29 11:00:08 +0530

    Added routes,controller of projects

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -5,59 +5,68 @@ import User from "../schema/user.js";
 import { NotFoundException } from "../exceptions/not-found.js";
 
 import { BadRequestException } from "../exceptions/bad-request.js";
-import {InternalException} from "../exceptions/internal-exception.js";
+import { InternalException } from "../exceptions/internal-exception.js";
 import { ErrorCode } from "../exceptions/root.js";
 
-
 export const createProject = async (req, res) => {
-    let user = await User.findById(req.user.id);
-    if(!user){
-      throw new NotFoundException('User not found.',ErrorCode.USER_NOT_FOUND);
-    }
-    const details = {
-      pName: req.body.pName,
-      pDescription: req.body.pDescription,
-      StartDate: req.body.StartDate,
-      logo_img: req.body.logo_img,
-      githubUrl: req.body.githubUrl,
-      teamLeader: req.user?.id,
-      isArchived: req.body.isArchived,
-      liveUrl: req.body.liveUrl, //optional
-      Tags: req.body.Tags,
-    };
-    let newProject = await Project.create(details);
+  let user = await User.findById(req.user.id);
+  if (!user) {
+    throw new NotFoundException("User not found.", ErrorCode.USER_NOT_FOUND);
+  }
+  const details = {
+    pName: req.body.pName,
+    pDescription: req.body.pDescription,
+    StartDate: req.body.StartDate,
+    logo_img: req.body.logo_img,
+    githubUrl: req.body.githubUrl,
+    teamLeader: req.user?.id,
+    isArchived: req.body.isArchived,
+    liveUrl: req.body.liveUrl, //optional
+    Tags: req.body.Tags,
+  };
+  let newProject = await Project.create(details);
 
-    if(newProject) {
-      const validProjectId = new mongoose.Types.ObjectId(newProject.id);
-      user.adminAt.push(validProjectId);
-      user.save();
-    } else {
-      res.status(400).json({ message: "Project id doesn't store in user details" });
-    }
+  if (newProject) {
+    const validProjectId = new mongoose.Types.ObjectId(newProject.id);
+    user.adminAt.push(validProjectId);
+    user.save();
+  } else {
+    res
+      .status(400)
+      .json({ message: "Project id doesn't store in user details" });
+  }
 
-    if (newProject) {
-      res.json(newProject);
-    } else {
-      // res.status(400).json({ message: "Project is not created" });
-    throw new InternalException('Unable to create Project',ErrorCode.INTERNAL_EXCEPTION);
-    }
-  
+  if (newProject) {
+    res.json(newProject);
+  } else {
+    // res.status(400).json({ message: "Project is not created" });
+    throw new InternalException(
+      "Unable to create Project",
+      ErrorCode.INTERNAL_EXCEPTION
+    );
+  }
 };
- 
+
 export const getProjectDetails = async (req, res) => {
-    const { pid } = req.params;
+  const { pid } = req.params;
   let projectDetail;
   try {
     projectDetail = await Project.findById(pid);
   } catch (error) {
-    throw new InternalException('Unable to get Project details',ErrorCode.INTERNAL_EXCEPTION);
+    throw new InternalException(
+      "Unable to get Project details",
+      ErrorCode.INTERNAL_EXCEPTION
+    );
   }
 
-    if (projectDetail) {
-      res.json(projectDetail);
-    } else {
-      throw new NotFoundException('Project not found.',ErrorCode.PROJECT_NOT_FOUND);
-    }
+  if (projectDetail) {
+    res.json(projectDetail);
+  } else {
+    throw new NotFoundException(
+      "Project not found.",
+      ErrorCode.PROJECT_NOT_FOUND
+    );
+  }
 };
 
 export const updateProjectDetails = async (req, res) => {
@@ -65,7 +74,7 @@ export const updateProjectDetails = async (req, res) => {
     const { pid } = req.params;
     let projectdt = await Project.findById(pid);
     let tagarr = projectdt.Tags;
-    tagarr = [...tagarr,...req.body.Tags];
+    tagarr = [...tagarr, ...req.body.Tags];
     tagarr = [...new Set(tagarr)];
     const details = {
       pName: req.body.pName,
@@ -73,14 +82,13 @@ export const updateProjectDetails = async (req, res) => {
       logo_img: req.body.logo_img,
       isArchived: req.body.isArchived,
       liveUrl: req.body.liveUrl,
-      Tags: tagarr,  // give new tag
+      Tags: tagarr, // give new tag
     };
 
-    let updateProjectDetails = await Project.findByIdAndUpdate(
-      pid,
-      details,
-      { runValidators: true, new: true }
-    );
+    let updateProjectDetails = await Project.findByIdAndUpdate(pid, details, {
+      runValidators: true,
+      new: true,
+    });
 
     if (updateProjectDetails) {
       res.json({
@@ -99,7 +107,6 @@ export const updateProjectDetails = async (req, res) => {
 
 export const deleteProject = async (req, res) => {
   try {
-
     const { pid } = req.params;
 
     let deletedProject = await Project.findByIdAndDelete(pid);
@@ -108,180 +115,312 @@ export const deleteProject = async (req, res) => {
     let cid = new mongoose.Types.ObjectId(pid);
     admin.adminAt.pull(cid);
     await admin.save();
-    console.log("fkefnekfnke",admin,"dededed");
-    
+    console.log("fkefnekfnke", admin, "dededed");
+
     if (deletedProject) {
       res.json({
         message: "Deleted project",
       });
     } else {
-      throw new NotFoundException("Project not found",ErrorCode.PROJECT_NOT_FOUND)
+      throw new NotFoundException(
+        "Project not found",
+        ErrorCode.PROJECT_NOT_FOUND
+      );
     }
   } catch (error) {
     throw error;
   }
 };
 
-
 export const getProjectOverView = async (req, res) => {
   // try {
-    const { pid } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(pid)) {
-      console.log("\t\t\tInvalid");
-      throw new BadRequestException('Project id is invalid',ErrorCode.INVALID_PROJECTID );
-      // throw new BadRequestException("Project id is invalid",ErrorCode.INVALID_PROJECTID)
-      console.log("After bad request");
-      }else{
+  const { pid } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(pid)) {
+    console.log("\t\t\tInvalid");
+    throw new BadRequestException(
+      "Project id is invalid",
+      ErrorCode.INVALID_PROJECTID
+    );
+    // throw new BadRequestException("Project id is invalid",ErrorCode.INVALID_PROJECTID)
+    console.log("After bad request");
+  } else {
+    const objectId = new mongoose.Types.ObjectId(pid);
+    let projectDetail = await Project.findById(
+      objectId,
+      "liveUrl pName githubUrl"
+    );
 
-        const objectId = new mongoose.Types.ObjectId(pid);
-        let projectDetail = await Project.findById(objectId, "liveUrl pName githubUrl");
-        
-        if (projectDetail) {
-          res.json(projectDetail);
-        } else {
-          res
-          .status(400)
-          .json({ message: "Project with the given id doesn't exist" });
-        }
-      }
+    if (projectDetail) {
+      res.json(projectDetail);
+    } else {
+      res
+        .status(400)
+        .json({ message: "Project with the given id doesn't exist" });
+    }
+  }
 };
 
 export const getProjectMembers = async (req, res) => {
-    const { pid } = req.params;
-    
-    let getMemberIds = await Project.findById(pid, "teamMembers");
+  const { pid } = req.params;
 
-    let teamMemberDetails = await Promise.all(
-      getMemberIds.teamMembers.map(async (mem) => {
-        let memberDetail = await User.findById(mem.teamMember, "name");
-        if (memberDetail) {
-          return {
-            teamMember: mem.teamMember,
-            role: mem.roles,
-            name: memberDetail.name,
-          };
-        } else {
-          throw new NotFoundException('Memebr not found.',ErrorCode.USER_NOT_FOUND);
-        }
-      })
-    );
-    res.json(teamMemberDetails);
-  
+  let getMemberIds = await Project.findById(pid, "teamMembers");
+
+  let teamMemberDetails = await Promise.all(
+    getMemberIds.teamMembers.map(async (mem) => {
+      let memberDetail = await User.findById(mem.teamMember, "name");
+      if (memberDetail) {
+        return {
+          teamMember: mem.teamMember,
+          role: mem.roles,
+          name: memberDetail.name,
+        };
+      } else {
+        throw new NotFoundException(
+          "Memebr not found.",
+          ErrorCode.USER_NOT_FOUND
+        );
+      }
+    })
+  );
+  res.json(teamMemberDetails);
 };
 
 export const addProjectMembers = async (req, res) => {
-    const { pid } = req.params;
-    const mID = req.body.memberId;
-    if (!mongoose.Types.ObjectId.isValid(mID)) {
-      console.log("\t\t\tInvalid");
-      throw new BadRequestException('Project id is invalid',ErrorCode.INVALID_PROJECTID );
-      // throw new BadRequestException("Project id is invalid",ErrorCode.INVALID_PROJECTID)
-      console.log("After bad request");
-      }
-      const memberId = new mongoose.Types.ObjectId(mID);
-       
-    let user = await User.findById(memberId);
-    if(!user) {
-      throw new NotFoundException('User not found.',ErrorCode.USER_NOT_FOUND);
-    }
-    let project = await Project.findById(pid);
-    const existingMember = project.teamMembers.find(member => member.teamMember.toString() === memberId.toString());
-
-    if (existingMember) {
-      throw new BadRequestException('USER WITH GIVEN ID ALREADY EXIST',ErrorCode.USER_ALREADY_EXIST_IN_PROJECT);
-    }
-
-    let addMember = await Project.findByIdAndUpdate(
-      pid,
-      { $push: { teamMembers: { teamMember: memberId,roles:"Monk" } } },
-      { runValidators: true, new: true }
+  const { pid } = req.params;
+  const mID = req.body.memberId;
+  if (!mongoose.Types.ObjectId.isValid(mID)) {
+    console.log("\t\t\tInvalid");
+    throw new BadRequestException(
+      "Project id is invalid",
+      ErrorCode.INVALID_PROJECTID
     );
+    // throw new BadRequestException("Project id is invalid",ErrorCode.INVALID_PROJECTID)
+    console.log("After bad request");
+  }
+  const memberId = new mongoose.Types.ObjectId(mID);
 
-    if(addMember) {
-      let cpid = new mongoose.Types.ObjectId(pid);
-      user.memberOf.push(cpid);
-      await user.save();
-    }
+  let user = await User.findById(memberId);
+  if (!user) {
+    throw new NotFoundException("User not found.", ErrorCode.USER_NOT_FOUND);
+  }
+  let project = await Project.findById(pid);
+  const existingMember = project.teamMembers.find(
+    (member) => member.teamMember.toString() === memberId.toString()
+  );
 
-    if (addMember) {
-      res.json(addMember);
-    } else {
-      throw new InternalException('Unable to Add member to Project',ErrorCode.INTERNAL_EXCEPTION);
-    }
-  
+  if (existingMember) {
+    throw new BadRequestException(
+      "USER WITH GIVEN ID ALREADY EXIST",
+      ErrorCode.USER_ALREADY_EXIST_IN_PROJECT
+    );
+  }
+
+  let addMember = await Project.findByIdAndUpdate(
+    pid,
+    { $push: { teamMembers: { teamMember: memberId, roles: "Monk" } } },
+    { runValidators: true, new: true }
+  );
+
+  if (addMember) {
+    let cpid = new mongoose.Types.ObjectId(pid);
+    user.memberOf.push(cpid);
+    await user.save();
+  }
+
+  if (addMember) {
+    res.json(addMember);
+  } else {
+    throw new InternalException(
+      "Unable to Add member to Project",
+      ErrorCode.INTERNAL_EXCEPTION
+    );
+  }
 };
 
-
 export const giveRoleToProjectMembers = async (req, res) => {
-    const { pid } = req.params;
-    const memberId = req.body.memberId;
-    const roles = req.body.roles;
+  const { pid } = req.params;
+  const memberId = req.body.memberId;
+  const roles = req.body.roles;
 
-    let project = await Project.findById(pid);
+  let project = await Project.findById(pid);
 
-    if (!project) {
-      throw new NotFoundException('Project not found.',ErrorCode.PROJECT_NOT_FOUND);
-    }
-
-
-    let teamMember = project.teamMembers.find((tm) =>
-      tm.teamMember.equals(memberId)
+  if (!project) {
+    throw new NotFoundException(
+      "Project not found.",
+      ErrorCode.PROJECT_NOT_FOUND
     );
+  }
 
-    if (!teamMember) {
-      throw new NotFoundException('Member not found.',ErrorCode.USER_NOT_FOUND);
-    }
+  let teamMember = project.teamMembers.find((tm) =>
+    tm.teamMember.equals(memberId)
+  );
 
-    teamMember.roles = roles;
+  if (!teamMember) {
+    throw new NotFoundException("Member not found.", ErrorCode.USER_NOT_FOUND);
+  }
 
-    const updatedProject = await project.save();
+  teamMember.roles = roles;
 
-    if (!updatedProject) {
-      throw new InternalException('Unable to assign role',ErrorCode.INTERNAL_EXCEPTION);
-    }
+  const updatedProject = await project.save();
 
-    res.json(updatedProject);
+  if (!updatedProject) {
+    throw new InternalException(
+      "Unable to assign role",
+      ErrorCode.INTERNAL_EXCEPTION
+    );
+  }
+
+  res.json(updatedProject);
 };
 
 export const removeProjectMembers = async (req, res) => {
-  
-    const { pid } = req.params;
-    const memberId = req.body.memberId;
+  const { pid } = req.params;
+  const memberId = req.body.memberId;
 
-    let project = await Project.findById(pid);
+  let project = await Project.findById(pid);
 
-    if (!project) {
-      throw new NotFoundException('Project not found.',ErrorCode.PROJECT_NOT_FOUND);
-    }
-
-    let member = await User.findById(memberId);
-    if(!member) {
-      throw new NotFoundException('Member not found.',ErrorCode.MEMEBER_NOT_FOUND);
-    }
-
-    const existingMember = project.teamMembers.find(mem => mem.teamMember.toString() === memberId.toString());
-    if(!existingMember) {
-      throw new NotFoundException('Given member not exist in project.',ErrorCode.MEMEBER_NOT_FOUND);
-    }
-
-    let updatedProject = await Project.findByIdAndUpdate(
-      pid,
-      { $pull: { teamMembers: { teamMember: memberId } } },
-      { runValidators: true, new: true }
+  if (!project) {
+    throw new NotFoundException(
+      "Project not found.",
+      ErrorCode.PROJECT_NOT_FOUND
     );
+  }
 
-    if(updatedProject) {
-      let user = await User.findById(memberId);
-      let cpid = new mongoose.Types.ObjectId(pid);
-      user.memberOf.pull(cpid);
-      await user.save()
-    } else {
-      throw new InternalException('Unable to remove projectid in user detail',ErrorCode.INTERNAL_EXCEPTION);
+  let member = await User.findById(memberId);
+  if (!member) {
+    throw new NotFoundException(
+      "Member not found.",
+      ErrorCode.MEMEBER_NOT_FOUND
+    );
+  }
+
+  const existingMember = project.teamMembers.find(
+    (mem) => mem.teamMember.toString() === memberId.toString()
+  );
+  if (!existingMember) {
+    throw new NotFoundException(
+      "Given member not exist in project.",
+      ErrorCode.MEMEBER_NOT_FOUND
+    );
+  }
+
+  let updatedProject = await Project.findByIdAndUpdate(
+    pid,
+    { $pull: { teamMembers: { teamMember: memberId } } },
+    { runValidators: true, new: true }
+  );
+
+  if (updatedProject) {
+    let user = await User.findById(memberId);
+    let cpid = new mongoose.Types.ObjectId(pid);
+    user.memberOf.pull(cpid);
+    await user.save();
+  } else {
+    throw new InternalException(
+      "Unable to remove projectid in user detail",
+      ErrorCode.INTERNAL_EXCEPTION
+    );
+  }
+
+  if (updatedProject) {
+    res.json(updatedProject);
+  } else {
+    throw new InternalException(
+      "Unable remove member from project",
+      ErrorCode.INTERNAL_EXCEPTION
+    );
+  }
+};
+
+export const getAllProjectDetails = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    let user = await User.findById(userId);
+    if (!user) {
+      throw new NotFoundException("User not found.", ErrorCode.USER_NOT_FOUND);
     }
 
-    if (updatedProject) {
-      res.json(updatedProject);
-    } else {
-      throw new InternalException('Unable remove member from project',ErrorCode.INTERNAL_EXCEPTION);
+    let projects = await Project.find({
+      $and: [
+        { teamLeader: { $ne: userId } },
+        { "teamMembers.teamMember": { $ne: userId } },
+      ],
+    }).select("pName pDescription githubUrl liveUrl Tags");
+
+    if (!projects) {
+      throw new NotFoundException(
+        "Project not found.",
+        ErrorCode.PROJECT_NOT_FOUND
+      );
     }
+
+    res.json({ projects });
+  } catch (error) {
+    console.error(error);
+    throw new NotFoundException(
+      "Project not found.",
+      ErrorCode.PROJECT_NOT_FOUND
+    );
+  }
+};
+
+export const getProjectOfMember = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    let user = await User.findById(userId);
+    if (!user) {
+      throw new NotFoundException("User not found.", ErrorCode.USER_NOT_FOUND);
+    }
+
+    let projects = await Project.find({
+      "teamMembers.teamMember": userId,
+    }).select("pName pDescription githubUrl liveUrl Tags");
+
+    if (!projects) {
+      throw new NotFoundException(
+        "Project not found.",
+        ErrorCode.PROJECT_NOT_FOUND
+      );
+    }
+
+    res.json({ projects });
+  } catch (error) {
+    console.error(error);
+    throw new NotFoundException(
+      "Project not found.",
+      ErrorCode.PROJECT_NOT_FOUND
+    );
+  }
+};
+
+export const getProjectOfAdmin = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    let user = await User.findById(userId);
+    if (!user) {
+      throw new NotFoundException("User not found.", ErrorCode.USER_NOT_FOUND);
+    }
+
+    let projects = await Project.find({
+      "teamLeader": userId,
+    }).select("pName pDescription githubUrl liveUrl Tags");
+
+    if (!projects) {
+      throw new NotFoundException(
+        "Project not found.",
+        ErrorCode.PROJECT_NOT_FOUND
+      );
+    }
+
+    res.json({ projects });
+  } catch (error) {
+    console.error(error);
+    throw new NotFoundException(
+      "Project not found.",
+      ErrorCode.PROJECT_NOT_FOUND
+    );
+  }
 };
--- src/routes/project.js
+++ src/routes/project.js
@@ -1,35 +1,77 @@
-import { Router } from "express"
-import authMiddleware from "../middlewares/auth.js"
+import { Router } from "express";
+import authMiddleware from "../middlewares/auth.js";
 import adminMiddleware from "../middlewares/admin.js";
-import { createProject,addProjectMembers,deleteProject,getProjectDetails,getProjectMembers,getProjectOverView,giveRoleToProjectMembers,removeProjectMembers,updateProjectDetails } from "../controllers/project.js"
-import { errorHandler } from "../errors/error-handler.js"
+import {
+  createProject,
+  addProjectMembers,
+  deleteProject,
+  getProjectDetails,
+  getProjectMembers,
+  getProjectOverView,
+  giveRoleToProjectMembers,
+  removeProjectMembers,
+  updateProjectDetails,
+  getAllProjectDetails,
+  getProjectOfMember,
+  getProjectOfAdmin,
+} from "../controllers/project.js";
+import { errorHandler } from "../errors/error-handler.js";
 
-const projectRoutes=Router()
+const projectRoutes = Router();
 
 // userRoutes.post('/',[authMiddleware,])
 
-
 // userRoutes.get('/',[authMiddleware])
 
-
 // projectRoutes.post('/', (createProject))
-projectRoutes.post('/', [authMiddleware],errorHandler(createProject))
+projectRoutes.post("/", [authMiddleware], errorHandler(createProject));
+projectRoutes.get("/all", [authMiddleware], errorHandler(getAllProjectDetails));
+projectRoutes.get("/memberOf",[authMiddleware],errorHandler(getProjectOfMember));
+projectRoutes.get("/adminAt",[authMiddleware],errorHandler(getProjectOfAdmin));
 // projectRoutes.get('/:pid', (getProjectDetails))
-projectRoutes.get('/:pid', [authMiddleware,adminMiddleware],errorHandler(getProjectDetails))
-projectRoutes.put('/:pid', (updateProjectDetails))   //updated project will be in request
+projectRoutes.get(
+  "/:pid",
+  [authMiddleware, adminMiddleware],
+  errorHandler(getProjectDetails)
+);
+projectRoutes.put("/:pid", updateProjectDetails); //updated project will be in request
 // projectRoutes.put('/:pid', [authMiddleware,adminMiddleware],errorHandler(updateProjectDetails))
 // projectRoutes.delete('/:pid', [](deleteProject))
-projectRoutes.delete('/:pid', [authMiddleware,adminMiddleware],errorHandler(deleteProject))
+projectRoutes.delete(
+  "/:pid",
+  [authMiddleware, adminMiddleware],
+  errorHandler(deleteProject)
+);
 // projectRoutes.get('/overview/:pid', [authMiddleware],(getProjectOverView))
-projectRoutes.get('/overview/:pid', [authMiddleware],errorHandler(getProjectOverView))
+projectRoutes.get(
+  "/overview/:pid",
+  [authMiddleware],
+  errorHandler(getProjectOverView)
+);
 // projectRoutes.get('/members/:pid',[authMiddleware,adminMiddleware],(getProjectMembers))
-projectRoutes.get('/members/:pid', [authMiddleware,adminMiddleware],errorHandler(getProjectMembers))
+projectRoutes.get(
+  "/members/:pid",
+  [authMiddleware, adminMiddleware],
+  errorHandler(getProjectMembers)
+);
 
 // projectRoutes.put('/addmembers/:pid', (addProjectMembers))
-projectRoutes.put('/addmembers/:pid', [authMiddleware,adminMiddleware],errorHandler(addProjectMembers))   
+projectRoutes.put(
+  "/addmembers/:pid",
+  [authMiddleware, adminMiddleware],
+  errorHandler(addProjectMembers)
+);
 // projectRoutes.put('/assignRole/:pid', (giveRoleToProjectMembers))
-projectRoutes.put('/assignRole/:pid', [authMiddleware,adminMiddleware],errorHandler(giveRoleToProjectMembers))
+projectRoutes.put(
+  "/assignRole/:pid",
+  [authMiddleware, adminMiddleware],
+  errorHandler(giveRoleToProjectMembers)
+);
 // projectRoutes.put('/removeMember/:pid', (removeProjectMembers))
-projectRoutes.put('/removeMember/:pid', [authMiddleware,adminMiddleware],errorHandler(removeProjectMembers))
+projectRoutes.put(
+  "/removeMember/:pid",
+  [authMiddleware, adminMiddleware],
+  errorHandler(removeProjectMembers)
+);
 
 export default projectRoutes;
