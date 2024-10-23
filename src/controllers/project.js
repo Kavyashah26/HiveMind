d19549d4ebe90c3ac18243814bfd3bfd0082ece3
Kavya Shah <kavyaatulshahpatan@gmail.com>
2024-10-23 20:56:46 +0530

    redeploy

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -100,7 +100,6 @@ export const updateProjectDetails = async (req, res) => {
      Tags: tagarr, // give new tag
    };
 
-
    let updateProjectDetails = await Project.findByIdAndUpdate(pid, details, {
      runValidators: true,
      new: true,
